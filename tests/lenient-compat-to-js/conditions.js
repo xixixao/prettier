a09d3efe4d95c842849ce80ed4fc4702251a45c7
Michal Srb <srb@fb.com>
2018-04-27 01:10:01 -0700

    Added test for fixed indented block parsing

---
+++ tests/lenient-compat-to-js/conditions.js
@@ -0,0 +1,13 @@
+
+if aouhasduosahdouasdhsaoudhsaduosahdousahdsaoudhsaoduh &&
+    asdhousadhsaoudhsoaudhsaoudh
+  f()
+else if aouhasduosahdouasdhsaoudhsaduosahdousahdsaoudhsaoduh &&
+    asdhousadhsaoudhsoaudhsaoudh
+  g()
+
+if f(
+      aouhasduosahdouasdhsaoudhsaduosahdousahdsaoudhsaoduh &&
+        asdhousadhsaoudhsoaudhsaoudh
+    )
+  f()
