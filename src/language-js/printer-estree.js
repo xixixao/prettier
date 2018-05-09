cdae0388c84bc74e5500a54d4e6d2395269b66fe
xixixao <xixixao@seznam.cz>
2018-05-09 02:36:20 +0100

    Fixed printing parens around catch clause

--- src/language-js/printer-estree.js
+++ src/language-js/printer-estree.js
@@ -1791,8 +1791,11 @@ function printPathNoParens(path, options, print, args) {
       ]);
     case "CatchClause":
       return concat([
-        "catch ",
-        n.param ? concat(["(", path.call(print, "param"), ") "]) : "",
+        "catch",
+        n.param
+          ? blockArgument(options, n.body, path.call(print, "param"))
+          : "",
+        " ",
         path.call(print, "body")
       ]);
     case "ThrowStatement":
